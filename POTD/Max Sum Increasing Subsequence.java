f56481c047b30cb6679da4190bde3dba4e66180e
Ronit-kumar-swain-07 <kumarswainronit@gmail.com>
2025-11-25 20:20:12 +0530

    commit

---
+++ POTD/Max Sum Increasing Subsequence.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public int maxSumIS(int arr[]) {
+        // code here
+        int n = arr.length;
+        int[] dp = new int[n];
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}

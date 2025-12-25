a3d6bdb7c51da034b82c9f4634b4e2b0b82c12c6
Ronit-kumar-swain-07 <kumarswainronit@gmail.com>
2025-12-25 16:46:00 +0530

    commit

---
+++ POTD/Peak Element.java
@@ -0,0 +1,10 @@
+/*
+Find the Peak Element in a 2D Matrix
+
+Given a 2D matrix mat[][], identify any peak element within the matrix.
+
+An element is considered a peak if it is greater than or equal to its four immediate neighbors: top, bottom, left, and right. For corner and edge elements, any missing neighbors are treated as having a value of negative infinity.
+
+Note: A peak element is not necessarily the global maximum, it only needs to satisfy the condition relative to its adjacent elements. Multiple peak elements may exist, return any one of them.
+Note that the driver code will print true if you return the correct position of peak element, else it will print false.
+*/

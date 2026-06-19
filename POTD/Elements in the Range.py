6fb393b9b62ea8d4f44717751814e9b63562dd44
Ronit-kumar-swain-07 <kumarswainronit@gmail.com>
2026-06-19 21:16:49 +0530

    Commit

--- POTD/Elements in the Range.py
+++ POTD/Elements in the Range.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Elements in the Range
 
 # Given an array arr[] containing distinct positive integers, and two integers start and end defining a range. Determine if the array contains all elements within inclusive range [start, end].
